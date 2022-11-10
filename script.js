ad1ebab988386fdc2615f6271b238fe9f10494b8
Simiek <samensah036@st.ug.edu.gh>
2022-11-10 20:23:52 +0000

    Added logic for all three hands

--- script.js
+++ script.js
@@ -1,10 +1,22 @@
+
 secondHand = document.querySelector(".second-hand")
+minuteHand = document.querySelector(".min-hand")
+hourHand = document.querySelector(".hour-hand")
 
 function setDate(){
     const now = new Date()
+
     const seconds = now.getSeconds()
-    degree = seconds/60 * 360
-    secondHand.style.transform = `rotate(${degree}deg)`
+    secondsDegree = (seconds/60 * 360) + 90
+    secondHand.style.transform = `rotate(${secondsDegree}deg)`
+
+    const minutes = now.getMinutes()
+    minutesDegree = (minutes/60 * 360) + 90
+    minuteHand.style.transform = `rotate(${minutesDegree}deg)`
+    
+    const hours = now.getHours()
+    hoursDegree = (hours/12 * 360) + 90
+    hourHand.style.transform = `rotate(${hoursDegree}deg)`
 
 }
 
